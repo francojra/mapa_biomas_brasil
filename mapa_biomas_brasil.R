0ba9b74dca668da558015d3cfa3dd1f7c78b562b
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-04-28 21:27:04 -0300

    Add files via upload

--- mapa_biomas_brasil.R
+++ mapa_biomas_brasil.R
@@ -39,24 +39,27 @@ m <- ggplot() +
   geom_sf(data = my_br) +
   geom_sf(data = my_biom, aes(fill = Bioma), color = "#f7fcb9") +
   geom_sf_text(data = my_br, aes(label = SIGLA_UF), 
-               size = 2, fontface = "bold", color = "#f7fcb9") +
+               size = 2.2, fontface = "bold", color = "#f7fcb9") +
   scale_fill_manual(values = c("#1E5A46", "#B38711", "#AF4F2F", 
                                "#1E395F", "#59385C", "#732F30")) +
   annotation_north_arrow(style = 
                 north_arrow_nautical(text_size = 7.5,
-                          text_face = "bold", family = "serif"),
+                          text_face = "bold"),
                          width = unit(1, "cm"),
                          height = unit(1, "cm")) +
   annotation_scale(location = "br", text_face = "bold",
-                   height = unit(0.15, "cm"), family = "serif") +
-  labs(x = "Longitude", y = "Latitude", 
-       title = "Biomas do Brasil", fill = "") +
+                   height = unit(0.15, "cm")) +
+  labs(x = "Longitude", y = "Latitude", fill = "") +
+  ggtitle("Biomas do Brasil") +
   theme_minimal() +
   theme(axis.text = element_text(color = "black", 
                           face = "bold", size = 8,
                           family = "serif"),
         axis.title = element_text(color = "black", 
-                           face = "bold", size = 9,
+                           face = "bold", size = 11,
+                           family = "serif"),
+        title = element_text(color = "black", 
+                           face = "bold", size = 11,
                            family = "serif"),
         legend.position = c(0.88, 0.24),
         legend.text = element_text(family = "serif"))
